d35fb0a0772e3bb90f08d6a9d2c1cd4b3dab2ee4
Henry Dang <phuongminh.dang@gmail.com>
2024-07-22 17:09:50 +0700

    Updated gw1n configuration

--- examples/gw1n_config/main.c
+++ examples/gw1n_config/main.c
@@ -3,6 +3,7 @@
 #include "usbd_core.h"
 #include "bflb_mtimer.h"
 #include "board.h"
+#include "gw1n_image.h"
 
 #define GPIO_LED GPIO_PIN_27
 
@@ -129,7 +130,7 @@ void gowin_download_bitstream(uint8_t *data, uint32_t len)
 
 int main(void)
 {
-    uint32_t fpga_id;
+    uint32_t data;
 
     board_init();
     gpio_led_init();
@@ -140,21 +141,21 @@ int main(void)
 
     /* Wait for fpga power is stable */
     bflb_mtimer_delay_ms(200);
-    fpga_id = gowin_read(0x11000000);
+    data = gowin_read(0x11000000);
 
-    if(fpga_id != 0x900281B) {
-    	printf("Error! Invalid device ID %X\r\n", fpga_id);
+    if(data != 0x900281B) {
+    	printf("Error! Invalid device ID %X\r\n", data);
     	printf("Exit program\r\n");
     	return;
     } else {
-    	printf("Found device ID %X\r\n", fpga_id);
+    	printf("Found device ID %X\r\n", data);
     }
 
     /* Write enable */
     gowin_write_cmd2(0x1500);
 
     /* Write bitstream */
-    //gowin_download_bitstream((uint8_t*)_acIOBUF, sizeof(_acIOBUF)-1);
+    gowin_download_bitstream((uint8_t*)gw1n_image, sizeof(gw1n_image));
 
     /* Write disable */
     gowin_write_cmd2(0x3A00);
@@ -162,6 +163,17 @@ int main(void)
     /* Write nop */
     gowin_write_cmd1(0x02);
     bflb_mtimer_delay_ms(10);
+
+#if 0 /* Because we configured SSPI as GPIO pins so at the end we cannot access the SSPI bus */
+    /* Validate the downloaded bit stream */
+    data = gowin_read(0x41000000);
+
+    if (data != 0x1F020) {
+        printf("Error! Bit stream download failed %X\r\n", data);
+    } else {
+        printf("Bit stream download successfully\r\n");
+    }
+#endif    
 
     cdc_acm_init();
 
