c9e6f1f1cf25b7e3768a102a6290d1a88bb4634c
Henry Dang <henrydang@mijoconnected.com>
2025-04-18 16:19:42 +0700

    Correct FPGA gatewave file name -> test_spi_fifo_gw1n_fpga_evb.py

--- examples/spififo_test/bitstream_to_c.sh
+++ examples/spififo_test/bitstream_to_c.sh
@@ -2,7 +2,7 @@
 
 # Convert GW1N bitstream to C header file
 # Fof the project.bin, refer to the repo litex-soc-builder and the design file in:
-# litex-soc-builder/custom_projects/power_monitor_gw1n_fpga_evb.py
+# litex-soc-builder/custom_projects/test_spi_fifo_gw1n_fpga_evb.py
 
 ./tools/bin2uint8_t.exe project.bin gw1n_image.h
 
