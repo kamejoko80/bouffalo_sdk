7400b747f92e0d9aafa0f100b25f90a2d9aeee0f
Henry Dang <henrydang@mijoconnected.com>
2025-04-18 16:06:43 +0700

    Updated test ping pong with FPGA_MODULE_GW1N_LV1 successfully

--- examples/spififo_test/main.c
+++ examples/spififo_test/main.c
@@ -38,38 +38,42 @@ int main(void)
     /* configure fpga */
     gowin_fpga_config();
 #endif
-    
+
 #if defined(FPGA_MODULE_LMCXO2_640HC)
     /* configure fpga */
     lmcxo2_fpga_config();
-    
+
     //extern int xsvftool_blk616_scan(void);
     //uint32_t idcode = xsvftool_blk616_scan();
-    
+
     //usb_printf("idcode=0x%08lx, revision=0x%01lx, part=0x%04lx, manufactor=0x%03lx\r\n", idcode,
     //    (idcode >> 28) & 0xf, (idcode >> 12) & 0xffff, (idcode >> 1) & 0x7ff);
 
-#endif    
-    
+#endif
+
+#if defined(FPGA_MODULE_GW1N_LV1)
     /* wait for fpga configured */
-    //spi_fifo_interface_bus_init(32);    
-    //bflb_mtimer_delay_ms(300);
-    //spi_ctrl_cmd_read_gw_version();
-    //spi_ctrl_cmd_read_chip_id();    
-    
+    spi_fifo_interface_bus_init(32);
+    bflb_mtimer_delay_ms(300);
+    spi_ctrl_cmd_read_gw_version();
+    spi_ctrl_cmd_read_chip_id();
+#endif
+
 #else /* MCU_MODULE_B */
 
     usb_printf("Module B: start program...\r\n");
 
+#if defined(FPGA_MODULE_GW1N_LV1)
     /* Init module b spi */
-    spi_fifo_interface_bus_init();
+    spi_fifo_interface_bus_init(32);
 
     /* Wait for fpga power is stable */
     bflb_mtimer_delay_ms(200);
+#endif
 
 #endif
 
-#if 0 /* ping pong test */
+#if defined(FPGA_MODULE_GW1N_LV1) /* ping pong test */
     printf("Press button to read GW version\r\n");
     while(!bflb_gpio_read(gpio, BOOT_PIN));
     while(bflb_gpio_read(gpio, BOOT_PIN));
